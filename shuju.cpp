52d18543387c31d87a0b93b2e6659eda58ab118f
Eason20110215 <3379282637@qq.com>
2024-05-31 23:34:10 +0800

    1

--- shuju.cpp
+++ shuju.cpp
@@ -5,21 +5,19 @@ int main()
 {
 	int n,a=1;
 	float pingjun=0,zhongwei=0,fangcha=0;
-	cout<<"请输入数据的个数:";
 	cin>>n;
 	cout << endl; 
 	const int n1=n;
 	float shuju[n1];
 	while(a<=n)
 	{
-		cout <<"请输入第"<<a<<"个数据："; 
 		cin >> shuju[a-1];
 		pingjun=pingjun+shuju[a-1];
 		cout<<endl;
 		a++;
 	}
 	pingjun=pingjun/n;
-	cout<<"平均数为"<<pingjun<<endl;
+	cout<<pingjun<<endl;
 	if(n%2==0)
 	{
 		zhongwei=(shuju[n/2]+shuju[(n/2)+1])/2;
@@ -28,7 +26,7 @@ int main()
 	{
 		zhongwei=shuju[(n-1)/2];
 	}
-	cout << "中位数为"<<zhongwei<<endl;
+	cout <<zhongwei<<endl;
 	a=1;
 	while(a<=n)
 	{
@@ -36,6 +34,6 @@ int main()
 		a++;
 	}
 	fangcha=(1/n)*fangcha;
-	cout << "方差为"<<fangcha<<endl;
+	cout<<fangcha<<endl;
 	return 0;
 }
