76fb58b5fc32cf4b0b56efc394780e2481864ed9
Eason20110215 <3379282637@qq.com>
2024-06-06 22:53:44 +0800

    1

---
+++ oj/1054.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int a,b=1,total=0;
+	cin>>a;
+	while(b<=a)
+	{
+		total=total+pow(b,2)
+		b++;
+	}
+	cout<<total;
+	return 0;
+}
