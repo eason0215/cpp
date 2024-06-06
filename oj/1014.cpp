dbb41d76fb3461edb7499e217e9415056109cc7a
Eason20110215 <3379282637@qq.com>
2024-06-06 23:02:19 +0800

    Create 1014.cpp

---
+++ oj/1014.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    double sum = 0.0;
+    for (int i = 1; i <= n; ++i) {
+        sum += 1.0 / i;
+    }
+
+    cout << fixed << setprecision(3) << sum <<endl;
+
+    return 0;
+}
