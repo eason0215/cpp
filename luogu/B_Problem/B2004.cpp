6ae35c35e1281dda677c671a4958d1a849406b5b
eason0215 <3379282637@qq.com>
2026-01-09 22:47:52 +0800

    Update

---
+++ luogu/B_Problem/B2004.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int a,b,c;
+int main()
+{
+	cin>>a>>b>>c;
+	cout<<setw(8)<<a<<" "<<setw(8)<<b<<" "<<setw(8)<<c<<endl;
+	return 0;
+}
---
+++ luogu/B_Problem/B2005.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cin>>c;
+    cout<<"  "<<c<<"  \n";
+    cout<<" "<<c<<c<<c<<" \n";
+    cout<<c<<c<<c<<c<<c<<"\n";
+    return 0;
+}
